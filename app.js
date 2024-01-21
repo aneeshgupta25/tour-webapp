c133763929f35add2bdd55ef0a97b584696b6d0c
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-01-21 22:22:30 +0530

    Environment Variables

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const tourRouter = require('./routes/tourRouter');
 const app = express();
 // middle-ware
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static(`${__dirname}/public`));
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
