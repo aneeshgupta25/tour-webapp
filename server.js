a5f15a16f01332c0f85fd7e79b7d838de787fd5b
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-01-24 15:20:15 +0530

    Adding document to mongodb

--- server.js
+++ server.js
@@ -21,6 +21,37 @@ mongoose
     console.log(err);
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = new mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'A forest hiker',
+  price: 497,
+  rating: 4.5
+});
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR: ', err);
+  });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
