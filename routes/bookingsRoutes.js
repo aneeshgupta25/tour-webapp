7533a73e2351dcf7e2bbabc385049df4c272e72f
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-02-28 03:52:11 +0530

    Additional routes for bookings

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -4,11 +4,19 @@ const bookingsController = require('./../controllers/bookingsController');
 
 const router = express.Router();
 
+router.use(authController.protect);
 // tourID is URL parameter
-router.get(
-  '/checkout-session/:tourID',
-  authController.protect,
-  bookingsController.getCheckoutSession,
-);
+router.get('/checkout-session/:tourID', bookingsController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+router
+  .route('/')
+  .get(bookingsController.getAllBookings)
+  .post(bookingsController.createBooking);
+router
+  .route('/:id')
+  .get(bookingsController.getBooking)
+  .patch(bookingsController.updateBooking)
+  .delete(bookingsController.deleteBooking);
 
 module.exports = router;
