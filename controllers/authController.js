b7e3f59037e3d3aa5c04fb07f451c96ab03d633f
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-02-28 16:40:43 +0530

    Production issue corrections 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,8 +15,8 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
-    secure: res.secure || res.headers['x-forwarded-proto'] === 'https',
-  };  
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
   res.cookie('jwt', token, cookieOptions);
 
   res.status(statusCode).json({
